84c7e3046f303c6211e2984f1fd20efa152c2f95
David <davca2322@gmail.com>
2021-08-10 15:29:58 -0400

    yes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let usernameElement = document.querySelector("#username");
 let messageElement = document.querySelector("#message");
 let button = document.querySelector("#submitButton");
+let messageContainer = document.querySelector(".allMessages");
+
 // Set database object REFERENCE here:
+
+let database = firebase.database().ref();
 
 /**
  * Updates the database with the username and message.
@@ -13,6 +17,26 @@ button.onclick = function updateDB(event){
     usernameElement.value = "";
     messageElement.value  = "";
     console.log(username + " : " + message);
+    
+    //Create a JSON object containung the username and message
+    let value = {
+        NAME: username,
+        MESSAGE: message,
+    }
+    
     // Update database here
+    database.push(value);
+    
+}
 
+// set database "child_added" event listener here
+database.on("child_added", addMessageToBoard);
+
+function addMessageToBoard(rowData) {
+    let row = rowData.val(); // returns an object just like the "value" we pushed
+    let username = row.NAME;
+    let message = row.MESSAGE;
+    let pElement = document.createElement("p");
+    pElement.innerHTML = username + ": " + message;
+    messageContainer.appendChild(pElement);
 }
